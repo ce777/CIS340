6ffccab39bb0ac24ddcf1cc9694b1d019ae7d9b9
ce777 <63565612+ce777@users.noreply.github.com>
2020-04-20 20:55:22 +0000

    Created third Project; Video 2

---
+++ MyThirdProject/App copy.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+function Student(props) {
+  return (
+    <View>
+      <Text>Hey, My Name is {props.name}, I am a student in CIS340!</Text>
+    </View>
+  );
+}
+
+export default function MultiComp(){
+  return(
+    <View style ={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text>Welcome to CIS340</Text>
+      <Student name="Chase Eslick" />
+      <Student name="Joshua Stout" />
+    </View>
+  );
+}
