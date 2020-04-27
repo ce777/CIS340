21701e7a0e22760090efe69dd39483c13d602a56
ce777 <63565612+ce777@users.noreply.github.com>
2020-04-26 18:05:39 -0600

    Week 14 Project 4

---
+++ MyFourthProject/App copy.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+
+export default class MyApp extends Component {
+  render() {
+  return (
+    <View style={{padding: 60}}>
+      <Text>Hello, I am here, I am student in CIs340!</Text>
+      <Text>I like using the class compenent!</Text>
+    </View>
+  );
+  }
+}
